1ce13fc162ca262a18f581bad9dccaffb0825446
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-02-24 21:48:45 -0300

    projeto criado

---
+++ src/main/java/br/com/gustavo/jpa_queries/JpaQueriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.jpa_queries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/jpa_queries/JpaQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.jpa_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
